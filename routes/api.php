7ae7973eb04b54758f69b816bccbb6aaec05028b
Gerson Maoze <gersonjfmaoze@gmail.com>
2023-09-14 01:28:41 +0300

    create login  methods

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,21 @@ use App\NivelAcademico;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
 });
 Route::get('/pelouros', 'profileController@getPelouros');
 Route::post('/employe/company-data', 'profileController@storeCompanyData');
